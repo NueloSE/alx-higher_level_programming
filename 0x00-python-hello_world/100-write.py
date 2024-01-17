027cd478886400dfc37486e4f22e1e1372a25e4a
Nuelo <akalo.emmanuel18@gmail.com>
2024-01-17 17:28:23 +0100

    Fix the exit value

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 import sys
-def _print(text):
-    sys.stderr.write(text)
-    exit(1)
-
-_print("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+exit(1)
