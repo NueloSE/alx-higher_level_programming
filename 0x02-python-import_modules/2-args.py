1929d81e6b671058e0b7eaf329448a83ff0e74b5
Nuelo <akalo.emmanuel18@gmail.com>
2024-01-19 03:39:05 +0100

    fix not executed when imported error

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-import sys
-argv = sys.argv
-count = 0
-for i in argv:
-    count += 1
-count -= 1
-print("{}".format(count), end=' ')
-if count == 0:
-    print("arguments.")
-elif count == 1:
-    print("argument:\n{}: Hello".format(count))
-else:
-    num = 1
-    print("arguments:")
-    for i in range(count):
-        print("{}: {}".format(num, argv[num]))
-        num += 1
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv
+    count = 0
+    for i in argv:
+        count += 1
+    count -= 1
+    print("{}".format(count), end=' ')
+    if count == 0:
+        print("arguments.")
+    elif count == 1:
+        print("argument:\n{}: Hello".format(count))
+    else:
+        num = 1
+        print("arguments:")
+        for i in range(count):
+            print("{}: {}".format(num, argv[num]))
+            num += 1
