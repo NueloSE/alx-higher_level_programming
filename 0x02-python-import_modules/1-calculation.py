c5a5c4d4142885479832db3973b48628346dcade
Nuelo <akalo.emmanuel18@gmail.com>
2024-01-19 03:02:29 +0100

    Fix import method

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-import calculator_1 as cal
+from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
     a = 10
     b = 5
-    addition = cal.add(a, b)
-    subtraction = cal.add(a, b)
-    multiplication = cal.mul(a, b)
-    division = cal.div(a, b)
+    addition = add(a, b)
+    subtraction = sub(a, b)
+    multiplication = mul(a, b)
+    division = div(a, b)
 
     print("{:d} + {:d} = {:d}".format(a, b, addition))
     print("{:d} - {:d} = {:d}".format(a, b, subtraction))
