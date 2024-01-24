8522c7edd361aa10f8a9371935dadcc951ce5a5b
Nuelo <akalo.emmanuel18@gmail.com>
2024-01-24 15:49:35 +0100

    Upload task 0

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    numRow = len(matrix)
+    if (numRow == 0) or ((len(matrix[0])) == 0):
+        return []
+
+    numCol = len(matrix[0])
+
+    matrixDouble = [[0] * numCol for i in range(numRow)]
+
+    for i in range(numRow):
+        for j in range(numCol):
+            matrixDouble[i][j] = (matrix[i][j] ** 2)
+
+    return (matrixDouble)
