a309cf2fc9d09f25eee5958fa4a35c5e18ba42fb
Nuelo <akalo.emmanuel18@gmail.com>
2024-02-07 08:55:23 +0100

    Change file to an executable file

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""This class is an empty class
+"""
+This class is an empty class
 The only function carried out by this class
 Is creates an instance of the class without
 any attribute or method.
