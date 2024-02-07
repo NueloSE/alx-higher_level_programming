a0ee58dfdc1908e0947cb216dd5f91744b87d6f4
Nuelo <akalo.emmanuel18@gmail.com>
2024-02-07 08:59:30 +0100

    Move the class documentation to the right possition

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""
-This class is an empty class
-The only function carried out by this class
-Is creates an instance of the class without
-any attribute or method.
-"""
-
 
 class Square():
+    """
+    This class is an empty class
+    The only function carried out by this class
+    Is creates an instance of the class without
+    any attribute or method.
+    """
     pass
