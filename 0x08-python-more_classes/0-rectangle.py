7737bfa2d288aa33088a392db9d5ba807e18aa81
Nuelo <akalo.emmanuel18@gmail.com>
2024-02-12 20:26:50 +0100

    Add python doc

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
+"""
+This module is a Rectangle module
+
+it will carryout various operations on a triangle
+
+triangle of different size and propertes
+"""
+
 
 class Rectangle:
+    """
+    This class is all about an object of rectangle type
+
+    it performs several operations on a triangle
+    """
     pass
