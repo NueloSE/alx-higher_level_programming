086635f96731665816cf650ecc2ef700109ea752
Nuelo <akalo.emmanuel18@gmail.com>
2024-02-20 12:21:20 +0100

    Upload task 0 and Readme.md files

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module contains the add_integer function
+The add_integer function computes the summation of two integer value
+It takes 2 arguments a and b
+a and b must be integer
+The function doesn't work with string"""
+
+def add_integer(a, b=98):
+    """it can work with just one argument and automatically set the second as 98
+    Returns:
+        a + b
+    """
+    if not (isinstance(a, int) | isinstance(a, float)):
+        raise TypeError("a must be an integer")
+    if not (isinstance(b, int) | isinstance(b, float)):
+        raise TypeError("b must be an integer")
+    return int(int(a) + int(b))
