f7c5d4d5e2df366fe99be83e8cd8a607d2fe6847
Nuelo <akalo.emmanuel18@gmail.com>
2024-02-22 19:44:00 +0100

    Upload task 9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+
+class Student:
+    """
+    This class represents a student with attributes such as
+    first name, last name and age.
+
+    Attributes:
+    first_name: A string representing the first name of the student.
+    last_name: A string representing the last name of thes student.
+    age: An integer representing the age of the student.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialize a student instance with firstname, lastname and age.
+        Args:
+            first_name: A string representing the first name of the student.
+            last_name: A string representing the last name of thes student.
+            age: An integer representing the age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Retrieve a dictionary representation of the Student instance.
+
+        Returns:
+            dict: A dictionary containing the serialized
+            attributes of the Student instance.
+        """
+        self.public_attribute = self.__dict__
+
+        self.json_dict = {
+            key: value for key, value in self.public_attribute.items()
+            if (isinstance(value, (list, str, int, bool)))
+        }
+
+        return (self.json_dict)
