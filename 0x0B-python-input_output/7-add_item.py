e0431cf193ac07df7ee5f727de3d3466289d5fa3
Nuelo <akalo.emmanuel18@gmail.com>
2024-02-22 16:37:53 +0100

    Update the module documentation

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+Simple adds command line argument in json format
+to a json file called add_item.json
+if add_item.json doesn't exit it creates it
+if it exist it appends to it.
+"""
 import sys
 loader = __import__('6-load_from_json_file').load_from_json_file
 dumper = __import__('5-save_to_json_file').save_to_json_file
