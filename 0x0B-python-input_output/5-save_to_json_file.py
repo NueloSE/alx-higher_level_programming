3ca3afb07bf37368a1ad8422bda7844106deb7c2
Nuelo <akalo.emmanuel18@gmail.com>
2024-02-22 04:35:46 +0100

    Update module and function doc string

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,5 +1,21 @@
 #!/usr/bin/python3
+""""
+Module name: 5-save_to_json_file.py
+
+Contains just one function that saves json file
+"""
 import json
+
+
 def save_to_json_file(my_obj, filename):
+    """
+    Converts the given JSON to a python obj.
+
+    Parameters:
+    my_string (JSON object): The object to be converted to python obj.
+
+    Returns:
+    obj: A python obj.
+    """
     with open(filename, "w", encoding="utf-8") as openFile:
         json_obj = json.dump(my_obj, openFile)
--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,5 +1,21 @@
 #!/usr/bin/python3
+""""
+Module name: 6-load_from_json_file
+
+Contains just one function that loads from json file
+"""
 import json
+
+
 def load_from_json_file(filename):
+    """
+    loads a json file.
+
+    Parameters:
+    my_string (JSON object): The object to be converted to python obj.
+
+    Returns:
+    obj: A python obj.
+    """
     with open(filename, "r", encoding='utf-8') as openFile:
         return (json.load(openFile))
