6045fb168629711d0568a8f630e1b6a3812993ff
Nuelo <akalo.emmanuel18@gmail.com>
2024-02-26 21:24:54 +0100

    Fix pep8 compliance issue

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Module name: ``3-is_kind_of_class``"""
+
+
 def is_kind_of_class(obj, a_class):
     """Check if an obj and a class have same kind"""
     return (isinstance(obj, a_class) | issubclass(type(obj), a_class))
