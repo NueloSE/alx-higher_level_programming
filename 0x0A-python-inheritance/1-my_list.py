ad2af6e44793173d55fd7248f6277337ec5f95c9
Nuelo <akalo.emmanuel18@gmail.com>
2024-02-26 13:20:18 +0100

    Upload task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Module title: `1-my_list`
+This module contains one function.
+"""
+
+
+class MyList(list):
+    """
+    A class that inherits the list module
+    contains some method
+    """
+    def print_sorted(self):
+        """
+    print_sorted:
+            arrange a list in a sorted form
+    Arguments:
+            1 unsorted iterable like a list
+
+    Return:
+            A sorted iterable
+
+    Raises:
+            None
+    """
+    print(sorted(self))
