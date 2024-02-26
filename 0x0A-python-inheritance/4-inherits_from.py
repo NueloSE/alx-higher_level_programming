bc9a27b22796e99b88c63567f189ad67b094fbdf
Nuelo <akalo.emmanuel18@gmail.com>
2024-02-26 22:10:36 +0100

    Update the function

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,4 +4,4 @@
 
 def inherits_from(obj, a_class):
     """checks if obj inherits class"""
-    return issubclass(type(obj), a_class)
+    return (issubclass(type(obj), a_class) and type(obj) is not a_class)
