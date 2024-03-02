f95dac1b98e301340de094c8e0fd0df69825d3e4
Nuelo <akalo.emmanuel18@gmail.com>
2024-03-02 02:38:36 +0100

    Update the base class to save and load from csv file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """Model name: ``base``"""
 import json
 import os
+import csv
 
 
 class Base:
@@ -70,3 +71,38 @@ class Base:
                     instance = cls.create(**jdict)
                     instances.append(instance)
                 return instances
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """Saves to a csv file"""
+        filename = cls.__name__ + '.csv'
+        with open(filename, 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            if cls.__name__ == 'Rectangle':
+                for obj in list_objs:
+                    writer.writerow([obj.id, obj.width,
+                                     obj.height, obj.x, obj.y])
+            elif cls.__name__ == 'Square':
+                for obj in list_objs:
+                    writer.writerow([obj.id, obj.size,
+                                     obj.height, obj.x, obj.y])
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """Loads obj from a csv"""
+        filename = cls.__name__ + '.csv'
+        instances = []
+        if not os.path.exists(filename):
+            return instances
+        with open(filename, 'r', newline='') as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                if cls.__name__ == 'Rectangle':
+                    instance = cls.create(id=int(row[0]), width=int(row[1]),
+                                          height=int(row[2]), x=int(row[3]),
+                                          y=int(row[4]))
+                elif cls.__name__ == "Square":
+                    instance = cls.create(id=int(row[0]), size=int(row[1]),
+                                          x=int(row[2]), y=int(row[3]))
+                instances.append(instance)
+        return instances
