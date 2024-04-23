1d2b3223398b0dff5f147e8d79d5ffd3b933a96b
Nuelo <akalo.emmanuel18@gmail.com>
2024-04-23 06:54:16 +0100

    fix: semistandard compliance issues

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -3,9 +3,10 @@
 const { argv } = require('process');
 
 if (argv[2]) {
-  for (let i = 2; argv[i] != undefined; i++) {
-    console.log(`${argv[i]}`);
-  }
+//   for (let i = 2; argv[i] != undefined; i++) {
+//     console.log(`${argv[i]}`);
+//   }
+  console.log(argv[2]);
 } else {
   console.log('No argument');
 }
