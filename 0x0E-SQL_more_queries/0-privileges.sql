fde56b6e9c0165f758243ef9499f95397ac97091
Nuelo <akalo.emmanuel18@gmail.com>
2024-04-30 18:50:09 +0100

    fix: handle error message raised

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- Shows all the privileges of the MYSQL user
-SHOW GRANTS FOR 'user_0d_1'@'localhost'
-SHOW GRANTS FOR 'user_0d_2'@'localhost'
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
