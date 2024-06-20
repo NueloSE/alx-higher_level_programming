cf56631e025b7006319e0ca5aa00bba4f8b51a09
Nuelo <akalo.emmanuel18@gmail.com>
2024-06-20 15:45:25 +0100

    feat: create scripts to generate an empty class

---
+++ 0x13-javascript_objects_scopes_closures/0-main.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const Rectangle = require('./0-rectangle');
+
+// class Rectangle {
+// 	constructor () {};
+// }
+
+const r1 = new Rectangle();
+console.log(r1);
+console.log(r1.constructor);
