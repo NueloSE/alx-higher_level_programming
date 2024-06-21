046bfb4f7b16dfdfa876f7f3643c209664d2071d
Nuelo <akalo.emmanuel18@gmail.com>
2024-06-21 10:42:30 +0100

    chore: remove unnecessary comments

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -4,11 +4,6 @@
 
 const list = require('./100-data').list;
 
-// const result = [];
-// for (let i = 0; i < list.length; i++) {
-//   result[i] = list[i] * i;
-// }
-
 const newList = list.map((x) => x * list.indexOf(x));
 
 console.log(list);
