111c5d9b9010107d3031da3b91563058cdb265cb
Nuelo <akalo.emmanuel18@gmail.com>
2024-06-21 10:33:28 +0100

    feat: implement list manipulation using map

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,11 +1,15 @@
 #!/usr/bin/node
 
+// A script that imports an array and computes a new array using map.
+
 const list = require('./100-data').list;
 
-const result = [];
-for (let i = 0; i < list.length; i++) {
-  result[i] = list[i] * i;
-}
+// const result = [];
+// for (let i = 0; i < list.length; i++) {
+//   result[i] = list[i] * i;
+// }
+
+const newList = list.map((x) => x * list.indexOf(x));
 
 console.log(list);
-console.log(result);
+console.log(newList);
