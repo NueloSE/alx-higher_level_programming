303ce7ac1a94eb91be5c3be99b138b8101228788
Nuelo <akalo.emmanuel18@gmail.com>
2024-06-21 23:14:39 +0100

    feat: script subquerry

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,15 @@
+
+-- Write a script that uses the hbtn_0d_tvshows database to lists all genres of the show Dexter.
+
+-- The tv_shows table contains only one record where title = Dexter (but the id can be different)
+-- Each record should display: tv_genres.name
+-- Results must be sorted in ascending order by the genre name
+-- You can use only one SELECT statement
+-- The database name will be passed as an argument of the mysql command
+
+SELECT
+	name
+FROM tv_show_genres tsg
+JOIN tv_genres tg ON tsg.genre_id = tg.id
+WHERE show_id = '8';
+ORDER BY name
---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,16 @@
+-- A script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.
+
+-- If a show doesn’t have a genre, display NULL in the genre column
+-- Each record should display: tv_shows.title - tv_genres.name
+-- Results must be sorted in ascending order by the show title and genre name
+-- You can use only one SELECT statement
+-- The database name will be passed as an argument of the mysql command
+
+SELECT
+	title,
+	name
+FROM tv_show_genres tsg
+JOIN tv_genres tg ON tsg.genre_id = tg.id
+RIGHT JOIN tv_shows ts ON tsg.show_id = ts.id
+ORDER BY title ASC;
+
