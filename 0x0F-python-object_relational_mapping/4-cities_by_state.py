440beb84eb35abedd3dded0ce17dac7093c85681
Nuelo <akalo.emmanuel18@gmail.com>
2024-06-23 20:46:21 +0100

    chore: fix pycodestyle compliance error

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -3,7 +3,8 @@
 """
 A script that lists all cities from the database hbtn_0e_4_usa
 
-Your script should take 3 arguments: mysql username, mysql password and database name
+Your script should take 3 arguments: mysql username,
+mysql password and database name
 You must use the module MySQLdb (import MySQLdb)
 Your script should connect to a MySQL server running on localhost at port 3306
 Results must be sorted in ascending order by cities.id
@@ -15,21 +16,21 @@ from sys import argv
 import MySQLdb
 
 if __name__ == "__main__":
-	db = MySQLdb.connect(
-		host="localhost",
-		port=3306,
-		user=argv[1],
-		passwd=argv[2],
-		db=argv[3],
-		charset="utf8"
-	)
-	cur = db.cursor()
-	cur.execute(
-		"SELECT cities.id, cities.name, states.name FROM cities \
-		JOIN states ON cities.state_id = states.id ORDER BY cities.id;"
-	)
-	querry_row = cur.fetchall()
-	for row in querry_row:
-		print(row)
-	cur.close()
-	db.close()
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        charset="utf8"
+    )
+    cur = db.cursor()
+    cur.execute(
+        "SELECT cities.id, cities.name, states.name FROM cities \
+        JOIN states ON cities.state_id = states.id ORDER BY cities.id;"
+    )
+    querry_row = cur.fetchall()
+    for row in querry_row:
+        print(row)
+    cur.close()
+    db.close()
