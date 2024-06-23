b6f636a4bd13fe65d8dc51353f6af81a4bf7d712
Nuelo <akalo.emmanuel18@gmail.com>
2024-06-23 20:23:17 +0100

    chore: fix pycodestyle error

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 """
-Once again, write a script that takes in arguments and displays all values in the states table of hbtn_0e_0_usa where name matches the argument. But this time, write one that is safe from MySQL injections!
+Once again, write a script that takes in arguments and
+displays all values in the states table of hbtn_0e_0_usa
+where name matches the argument. But this time,
+write one that is safe from MySQL injections!
 
-Your script should take 4 arguments: mysql username, mysql password, database name and state name searched (safe from MySQL injection)
+Your script should take 4 arguments: mysql username,
+mysql password, database name and state name searched
+(safe from MySQL injection)
 You must use the module MySQLdb (import MySQLdb)
 Your script should connect to a MySQL server running on localhost at port 3306
 Results must be sorted in ascending order by states.id
