150ce04732b10b51c128d35733436e13ba8ee727
Nuelo <akalo.emmanuel18@gmail.com>
2024-06-24 18:25:50 +0100

    feat: implement add a new row to table

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,1 +1,33 @@
 #!/usr/bin/python3
+"""
+A script that adds the State object “Louisiana” to the database hbtn_0e_6_usa
+
+Your script should take 3 arguments: mysql username,
+mysql password and database name
+You must use the module SQLAlchemy
+You must import State and Base from model_state -
+from model_state import Base, State
+Your script should connect to a MySQL server running on localhost at port 3306
+Print the new states.id after creation
+Your code should not be executed when imported
+"""
+
+from model_state import Base, State
+from sys import argv
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+if __name__ == "__main__":
+    querry = 'mysql+mysqldb://{}:{}@localhost/{}'
+    engine = create_engine(
+        querry.format(argv[1], argv[2], argv[3])
+    )
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_state = State(name="Louisiana")
+    session.add(new_state)
+
+    for state in session.query(State).filter(State.name == "Louisiana"):
+        print(state.id)
