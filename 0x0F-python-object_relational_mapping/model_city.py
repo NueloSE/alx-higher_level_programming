9a6a39e6d47c37ddd44009c3e864313e3b20c6c0
Nuelo <akalo.emmanuel18@gmail.com>
2024-06-25 07:30:17 +0100

    fix: add class doc and fix pycodestyle issues

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -20,21 +20,24 @@ from model_state import Base, State
 
 
 class City(Base):
-	__tablename__ = 'cities'
-	id = Column(
-		Integer,
-		autoincrement=True,
-		unique=True,
-		nullable=False,
-		primary_key=True
-	)
-	name = Column(
-		String(128),
-		nullable=False
-	)
-	state_id = Column(
-		Integer,
-		ForeignKey('states.id'),
-		nullable=False
-	)
-	state = relationship("State", backref="cities")
+    """
+    create a city class with its attribute to be linked to the database
+    """
+    __tablename__ = 'cities'
+    id = Column(
+        Integer,
+        autoincrement=True,
+        unique=True,
+        nullable=False,
+        primary_key=True
+    )
+    name = Column(
+        String(128),
+        nullable=False
+    )
+    state_id = Column(
+        Integer,
+        ForeignKey('states.id'),
+        nullable=False
+    )
+    state = relationship("State", backref="cities")
