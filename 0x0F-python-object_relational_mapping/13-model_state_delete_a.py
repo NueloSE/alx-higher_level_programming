11bbb734b89919ca763b1e6e7dc4a38517123db0
Nuelo <akalo.emmanuel18@gmail.com>
2024-06-25 05:09:26 +0100

    feat: implement script to delete from database using sqlalchemy

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+"""
+A script that deletes all State objects with a name containing
+the letter a from the database hbtn_0e_6_usa
+
+Your script should take 3 arguments: mysql username,
+mysql password and database name
+You must use the module SQLAlchemy
+You must import State and Base from model_state -
+from model_state import Base, State
+Your script should connect to a MySQL server
+running on localhost at port 3306
+Your code should not be executed when imported
+"""
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    querry = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
+    engine = create_engine(
+        querry.format(argv[1], argv[2], argv[3])
+    )
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(engine)
+    session = Session()
+    session.query(State).filter(State.name.like('%a%')).delete()
+    session.commit()
