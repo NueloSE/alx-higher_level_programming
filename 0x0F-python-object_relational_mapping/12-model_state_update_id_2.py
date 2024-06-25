b7ef73caaffdd4e056152d3715afe519d58bb179
Nuelo <akalo.emmanuel18@gmail.com>
2024-06-25 04:40:59 +0100

    fix: pycodestyle error

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 """
-A script that changes the name of a State object from the database hbtn_0e_6_usa
+A script that changes the name of a State object
+from the database hbtn_0e_6_usa
 
-Your script should take 3 arguments: mysql username, mysql password and database name
+Your script should take 3 arguments: mysql username,
+mysql password and database name
 You must use the module SQLAlchemy
-You must import State and Base from model_state - from model_state import Base, State
+You must import State and Base from model_state -
+from model_state import Base, State
 Your script should connect to a MySQL server running on localhost at port 3306
 Change the name of the State where id = 2 to New Mexico
 Your code should not be executed when imported
@@ -18,11 +21,11 @@ from model_state import Base, State
 if __name__ == "__main__":
     querry = 'mysql+mysqldb://{}:{}@localhost/{}'
     engine = create_engine(
-        querry.format(argv[1],argv[2],argv[3])
+        querry.format(argv[1], argv[2], argv[3])
     )
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for state in session.query(State).filter_by(id = 2):
+    for state in session.query(State).filter_by(id=2):
         state.name = 'New Mexico'
         session.commit()
