459b0a849fe57ff2dd4e0c6edb38f4590eb14e1d
Nuelo <akalo.emmanuel18@gmail.com>
2024-07-12 15:29:29 +0100

    feat: create script to get url using urllib

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+A python script that fetches https://alx-intranet.hbtn.io/status
+- You must use the package urllib
+- You are not allowed to import any packages other than urllib
+- The body of the response must be displayed like the following
+example (tabulation before -)
+- You must use a with statement
+"""
+
+import urllib.request
+
+if __name__ == "__main__":
+    urlToFetch = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(urlToFetch) as response:
+        html = response.read()
+    output = "Body response:\n\t- \
+            type: {}\n\t- \
+            content: {}\n\t- \
+            utf content: {}"
+    print(
+        output.format(type(html), html, html.decode('utf-8'))
+    )
