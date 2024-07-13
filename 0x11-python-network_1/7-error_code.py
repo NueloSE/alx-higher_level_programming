b9ce38b72ced7dafa8b8e37ce11602b0ec7853e4
Nuelo <akalo.emmanuel18@gmail.com>
2024-07-13 09:53:46 +0100

    chore: fix error formating

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """
-A Python script that takes in a URL, sends a request to the URL and displays the body of the response.
+A Python script that takes in a URL, sends a request to the URL and
+displays the body of the response.
 
-If the HTTP status code is greater than or equal to 400, print: Error code: followed by the value of the HTTP status code
+If the HTTP status code is greater than or equal to 400, print:
+Error code: followed by the value of the HTTP status code
 You must use the packages requests and sys
 You are not allowed to import packages other than requests and sys
 You don’t need to check arguments passed to the script (number or type)
@@ -15,7 +17,10 @@ import requests
 def main():
     url = argv[1]
     r = requests.get(url)
-    print(r.status_code)
+    if r.status_code == 200:
+        print('Regular request')
+    else:
+        print('Error code: {}'.format(r.status_code))
 
 
 if __name__ == "__main__":
